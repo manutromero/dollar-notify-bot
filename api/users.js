8195ddcbf0fc5ee414bfb37bb6498921ef58f117
Manuel Romero <manu8083el@gmail.com>
2022-10-15 18:54:26 -0500

    add set interval test

--- api/users.js
+++ api/users.js
@@ -1,12 +1,19 @@
 
 module.exports = (req, res) => {
     if (req.method  === "GET"){
-        res.json([
-            {name:"manuel", location: "bogota"}
-        ])
+
+        setTimeout((index)=>{
+            res.json([
+                {name:"manuel", location: "bogota", index: `${index}` }
+            ])
+        },100)
+
+        
     }else{
         const {name, location} = req.body;
 
         res.send({status: "User created", name, location})
     }
+
+    
 }
