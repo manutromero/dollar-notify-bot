e590c7f57247b17baa9d94f370af11a253c3465a
Manuel Romero <manu8083el@gmail.com>
2022-10-15 21:01:28 -0500

    Prueba Final

--- api/users.js
+++ api/users.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 module.exports = (req, res) => {
 
     const helloWord = async ()=>{
-            await axios.get('https://api.telegram.org/bot5509477086:AAElj-pA5Zg9KGT9AcrV--uPwK-OxF6peUg/sendMessage?chat_id=-802012750&text=Se envia cada 10 minutos').then(resp => {
+            await axios.get('https://api.telegram.org/bot5509477086:AAElj-pA5Zg9KGT9AcrV--uPwK-OxF6peUg/sendMessage?chat_id=-802012750&text=Este mensaje se dejara como prueba y se enviara cada hora').then(resp => {
     
                 console.log(resp.data);
 
                 res.send(
-                    {name:"manuel 3", location: "bogota" , message: resp.data}
+                    {name:"Manuel Romero", location: "bogota" , message: resp.data}
                 )
             });
     }
@@ -18,8 +18,5 @@ module.exports = (req, res) => {
 
     setInterval(()=>{
         helloWord()
-    },600000)
-
-
-    
+    },3600000)
 }
