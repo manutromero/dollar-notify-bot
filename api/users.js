c21d68b8fb9383a710357720d43a4e65516473ab
Manuel Romero <manu8083el@gmail.com>
2022-10-15 19:05:39 -0500

    add set interval test 4

--- api/users.js
+++ api/users.js
@@ -1,21 +1,16 @@
 
 module.exports = (req, res) => {
-    let candles = 0;
-    
+
+
     if (req.method  === "GET"){
 
         setTimeout(()=>{
             res.send(
                 {name:"manuel", location: "bogota", index: `${candles}` }
             )
-        },100)
+        },1000)
 
-        function updateCandles() {
-            candles += 1;
-            // recursive function, keep updating every second
-            setTimeout(updateCandles, 1000);
-        }
-        updateCandles();
+
     }else{
         const {name, location} = req.body;
 
