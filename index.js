991c58bea2c9fb846ac48d1cfb2d531e01d95d08
Manuel Romero <manu8083el@gmail.com>
2022-10-15 20:55:31 -0500

    se enviara cada 2 horas

--- index.js
+++ index.js
@@ -10,21 +10,6 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 
-  const helloWord = async ()=>{
-    // await axios.get('https://api.telegram.org/bot5509477086:AAElj-pA5Zg9KGT9AcrV--uPwK-OxF6peUg/sendMessage?chat_id=-802012750&text=Cada 10 minutos se envia este mensaje').then(resp => {
-
-    //     console.log(resp.data);
-
-      
-    // });
-}
-
-    helloWord()
-
-    setInterval(()=>{
-       helloWord()
-      },600000)
-
 });
 
 server.listen(port, hostname, () => {
