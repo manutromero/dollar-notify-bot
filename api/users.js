16f35c84144b14a30108ad298477a09854fd5af8
Manuel Romero <manu8083el@gmail.com>
2022-10-15 19:33:25 -0500

    Add set interval 2

--- api/users.js
+++ api/users.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 module.exports = (req, res) => {
 
     if (req.method  === "GET"){
-
-        setInterval(()=>{
             res.send(
-                {name:"manuel", location: "bogota" }
+                {name:"manuel 2", location: "bogota" }
             )
-            axios.get('https://api.telegram.org/bot5509477086:AAElj-pA5Zg9KGT9AcrV--uPwK-OxF6peUg/sendMessage?chat_id=-802012750&text=Ejecutando Intervalo desde server').then(resp => {
+        setInterval(()=>{
+           
+            axios.get('https://api.telegram.org/bot5509477086:AAElj-pA5Zg9KGT9AcrV--uPwK-OxF6peUg/sendMessage?chat_id=-802012750&text=Ejecutando Intervalo').then(resp => {
 
                 console.log(resp.data);
             });
