ee211150d7f0753c2a69eacc555a7a5c2c2c079f
Manuel Romero <manu8083el@gmail.com>
2022-10-16 23:25:50 -0500

    Add new commit

--- api/users.js
+++ api/users.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 module.exports =async (req, res) => {
 
-            await axios.get(`https://api.telegram.org/${process.env.TELEGRAM_HASH}/sendMessage?chat_id=${process.env.CHANNEL_ID}&text=usamos github actions`).then(resp => {
+            await axios.get(`https://api.telegram.org/${process.env.TELEGRAM_HASH}/sendMessage?chat_id=${process.env.CHANNEL_ID}&text=Pruebas desde vercel y github`).then(resp => {
     
                 console.log(resp.data);
 
