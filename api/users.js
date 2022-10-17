ade18244a50676d657db6d3568b1af7a3bb0c534
Manuel Romero <manu8083el@gmail.com>
2022-10-16 21:39:21 -0500

    mensaje

--- api/users.js
+++ api/users.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 module.exports =async (req, res) => {
 
-            await axios.get(`https://api.telegram.org/${process.env.TELEGRAM_HASH}/sendMessage?chat_id=${process.env.CHANNEL_ID}&text=Este mensaje se dejara como prueba y se enviara cada hora`).then(resp => {
+            await axios.get(`https://api.telegram.org/${process.env.TELEGRAM_HASH}/sendMessage?chat_id=${process.env.CHANNEL_ID}&text=usamos github actions`).then(resp => {
     
                 console.log(resp.data);
 
